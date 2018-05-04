2faaf0caea5b08c87f9510b5ebd4ddedb686ef28
Kat Sullivan <kmsullivan012@gmail.com>
2018-05-03 23:24:39 -0400

    fixed merge

--- main.js
+++ main.js
@@ -1,51 +1,6 @@
 // JAVASCRIPT FILE
-$(document).ready(function(){
-
-	
-	$("#myPara").click(function(){
-		$(this).hide();
-	});
-
-	$("#hide").click(function(){
-		$("#anotherPara").hide();
-	});
-
-	$("#cssPara").hover(function(){
-		$(this).css("background-color", "#c8b34f");
-	});
-
-	$("div").hide();
-
-	$("#show").click(function(){
-		// $("div").show()
-		$("div").fadeIn(5000)
-	})
-
-
-
-
-
-
-});
-=======
-var textOne = "this is some text";
-var textTwo = "kat needs a nap";
 
 $(document).ready(function(){
-
-	$("h2").text(textOne);
-	switchText();
-
-	function switchText() {
-		if($("h2").text() == textOne){
-			$("h2").text(textTwo);
-		} else {
-			$("h2").text(textOne);
-		}
-		setTimeout(switchText, 3000);
-	}
-
-
 
 	$('span').hide();
 
@@ -63,4 +18,3 @@ $(document).ready(function(){
 
 
 })
->>>>>>> dc3287ec04868a9e63ad4a76a0cabf72ecc28ce1
